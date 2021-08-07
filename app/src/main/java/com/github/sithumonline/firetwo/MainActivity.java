6f54811dc9ce686782460bb9e9a0e5da5c15f45b
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-08-07 16:48:18 +0530

    Dowgrade to 4.2.0

--- app/src/main/java/com/github/sithumonline/firetwo/MainActivity.java
+++ app/src/main/java/com/github/sithumonline/firetwo/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseApp.initializeApp(this);
+//        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
         setUpRecyclerView();
     }
